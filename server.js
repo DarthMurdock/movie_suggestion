bd7a79f93f4f2c8077dc4c8d55cd3c1afe830990
Philip Cowgill <philipcowgill@gmail.com>
2026-09-19 06:43:44 -0400

    Add SEO/social preview meta tags, robots.txt, sitemap, favicon

--- server.js
+++ server.js
@@ -374,6 +374,7 @@ const MIME_TYPES = {
   ".svg": "image/svg+xml",
   ".ico": "image/x-icon",
   ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
 };
 
 async function handleStatic(req, res, pathname) {
